9fe763531b416840e21001329947319e4ec7009b
Floozutter <me@floozutter.site>
2021-07-18 03:35:57 -0700

    fix Koy hashing

--- rdev/shiftry/src/koy.rs
+++ rdev/shiftry/src/koy.rs
@@ -1,4 +1,5 @@
 use std::hash::{Hash, Hasher};
+use std::mem::discriminant;
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct Koy(pub rdev::Key);
@@ -7,6 +8,12 @@ impl Eq for Koy {}
 
 impl Hash for Koy {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        0.hash(state);
+        discriminant(&self.0).hash(state);
+        match self.0 {
+            rdev::Key::Unknown(u) => {
+                u.hash(state);
+            },
+            _ => {},
+        }
     }
 }
