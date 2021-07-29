88e55c95a28d66b437d4eeca8fe5edc493f199d2
Floozutter <me@floozutter.site>
2021-07-29 09:00:02 -0700

    use if let instead of match

--- rdev/shiftry/src/koy.rs
+++ rdev/shiftry/src/koy.rs
@@ -9,11 +9,8 @@ impl Eq for Koy {}
 impl Hash for Koy {
     fn hash<H: Hasher>(&self, state: &mut H) {
         discriminant(&self.0).hash(state);
-        match self.0 {
-            rdev::Key::Unknown(u) => {
-                u.hash(state);
-            },
-            _ => {},
+        if let rdev::Key::Unknown(u) = self.0 {
+            u.hash(state);
         }
     }
 }
