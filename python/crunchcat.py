3b68dd096154e4bb2a77ea0f8d90bc11a58b1169
Floozutter <me@floozutter.site>
2021-08-21 04:45:53 -0700

    add parse_args

--- python/crunchcat.py
+++ python/crunchcat.py
@@ -1,1 +1,11 @@
 import ffmpeg
+from argparse import ArgumentParser
+from pathlib import Path
+
+def parse_args() -> Path:
+    parser = ArgumentParser()
+    parser.add_argument("rootpath", nargs = "?", type = Path, default = Path())
+    return parser.parse_args().rootpath
+
+if __name__ == "__main__":
+    parse_args()
