9152a3f5b76de08b00bbf6517e034b5ea78cb64d
Floozutter <me@floozutter.site>
2021-08-23 02:43:56 -0700

    take Iterable[Path] instead of root Path

--- python/crunchcat.py
+++ python/crunchcat.py
@@ -1,14 +1,15 @@
 import ffmpeg
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Iterable
 
 def parse_args() -> Path:
     parser = ArgumentParser()
     parser.add_argument("rootpath", nargs = "?", type = Path, default = Path())
     return parser.parse_args().rootpath
 
-def main(rootpath: Path) -> None:
+def main(paths: Iterable[Path]) -> None:
     pass
 
 if __name__ == "__main__":
-    main(parse_args())
+    main(parse_args().rglob("*"))
