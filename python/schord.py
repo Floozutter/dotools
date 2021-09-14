b4a43d046ab870104f270f56314f27417d43146d
Floozutter <me@floozutter.site>
2021-09-14 01:10:52 -0700

    finish schord.py

--- python/schord.py
+++ python/schord.py
@@ -12,17 +12,30 @@ from typing import Iterable
 WIDTH = 2
 MAX_VALUE = 2**(8*WIDTH - 1) - 1
 RATE = 44100
-A4 = 440
 
 def parse_args() -> tuple[tuple[float, ...], Path]:
     parser = ArgumentParser(description = __doc__)
-    parser.add_argument("pitches", nargs = "+", type = str)
+    parser.add_argument("pitches", nargs = "+", type = float)
+    parser.add_argument("-d", "--duration", type = float, default = 1)
+    parser.add_argument("-v", "--volume", type = float, default = 0.5)
     parser.add_argument("-o", "--outpath", type = str, required = True)
     args = parser.parse_args()
-    return tuple(args.pitches), args.outpath
+    if args.duration < 0:
+        parser.error("argument -d/--duration: must be nonnegative")
+    elif args.volume < 0 or args.volume > 1:
+        parser.error("argument -v/--volume: must be in [0, 1]")
+    else:
+        return tuple(args.pitches), args.duration, args.volume, args.outpath
 
-def main(pitches: Iterable[float], out: Path) -> None:
-    samples = ()
+def main(pitches: Iterable[float], duration: float, volume: float, out: Path) -> None:
+    voices = tuple(
+        tuple(
+            volume * math.sin(2*math.pi * pitch * i / RATE)
+            for i in range(int(RATE * duration))
+        )
+        for pitch in pitches
+    )
+    samples = tuple(map(sum, zip(*voices)))
     with wave.open(str(out), "w") as owave:
         owave.setnchannels(1)
         owave.setsampwidth(WIDTH)
@@ -30,6 +43,7 @@ def main(pitches: Iterable[float], out: Path) -> None:
         for sample in samples:
             value = int(MAX_VALUE * sample)
             owave.writeframes(pack("<h", value))
+    return voices
 
 if __name__ == "__main__":
     main(*parse_args())
