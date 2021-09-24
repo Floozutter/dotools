7c3b880a5fee9fc2ba58f1cde86ecbf5e0c5cb03
Floozutter <me@floozutter.site>
2021-09-24 14:48:43 -0700

    use argparse and add description

--- python/bitcrush.py
+++ python/bitcrush.py
@@ -1,6 +1,13 @@
+"""
+bitcrush WAVE files!
+"""
+
+
+from argparse import ArgumentParser
 from pathlib import Path
 
 def parse_args() -> tuple[Path, Path]:
+    parser = ArgumentParser(description = __doc__)
     return Path(), Path()
 
 def main(iwavepath: Path, owavePath: Path) -> None:
