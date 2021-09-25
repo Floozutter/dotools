70a888bcdd00a24355ec7108b6560fbe6ca95027
Floozutter <me@floozutter.site>
2021-09-25 14:41:26 -0700

    parse iwavepath and owavepath args

--- python/bitcrush.py
+++ python/bitcrush.py
@@ -2,15 +2,17 @@
 bitcrush WAVE files!
 """
 
-
 from argparse import ArgumentParser
 from pathlib import Path
 
 def parse_args() -> tuple[Path, Path]:
     parser = ArgumentParser(description = __doc__)
-    return Path(), Path()
+    parser.add_argument("iwavepath", type = Path)
+    parser.add_argument("owavepath", type = Path)
+    args = parser.parse_args()
+    return args.iwavepath, args.owavepath
 
-def main(iwavepath: Path, owavePath: Path) -> None:
+def main(iwavepath: Path, owavepath: Path) -> None:
     pass
 
 if __name__ == "__main__":
